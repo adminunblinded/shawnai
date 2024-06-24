9d0adaaf6b5836211cbaf05e21c8058923384c33
adminunblinded <158335079+adminunblinded@users.noreply.github.com>
2024-06-24 11:21:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
-from flask import Flask, request, render_template, send_file, jsonify
+from flask import Flask, request, render_template, jsonify
 import openai
-import requests
 import os
-import io
 import pickle
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 app = Flask(__name__)
 
-# Set your API keys here
+# Set your API key here
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-ELEVEN_LABS_API_KEY = os.getenv('ELEVEN_LABS_API_KEY')
 
 # Initialize OpenAI
-openai.api_key = OPENAI_API_KEY
-
-# Load your OpenAI API key
 openai.api_key = OPENAI_API_KEY
 
 # Function to split the text into chunks
@@ -83,37 +77,11 @@ def generate_relevant_response(prompt, chunk):
     )
     return response['choices'][0]['message']['content']
 
-
 # Function to generate a response from OpenAI
 def generate_response(prompt):
     # Find the most relevant chunk
     relevant_chunk = find_most_relevant_chunk(prompt, chunks)
     return generate_relevant_response(prompt, relevant_chunk)
-
-# Function to convert text to speech using Eleven Labs
-def text_to_speech(text):
-    voice_id = "2EiwWnXFnvU5JabPnv8n"  # Replace with the actual voice ID you want to use
-    url = f'https://api.elevenlabs.io/v1/text-to-speech/{voice_id}'
-    headers = {
-        'xi-api-key': ELEVEN_LABS_API_KEY,
-        'Content-Type': 'application/json',
-        'Accept': 'audio/mpeg'
-    }
-    data = {
-        "text": text,
-        "model_id": "eleven_monolingual_v1",
-        "voice_settings": {
-            "stability": 0.75,
-            "similarity_boost": 0.75
-        }
-    }
-    response = requests.post(url, headers=headers, json=data)
-    
-    if response.status_code == 200:
-        return io.BytesIO(response.content)
-    else:
-        print(f"Error: {response.status_code}, {response.text}")
-        return None
 
 @app.route('/')
 def index():
@@ -123,13 +91,7 @@ def index():
 def chat():
     user_input = request.form['user_input']
     bot_response = generate_response(user_input)
-    
-    audio_data = text_to_speech(bot_response)
-    
-    if audio_data:
-        return send_file(audio_data, mimetype='audio/mpeg', as_attachment=False, attachment_filename='response.mp3')
-    else:
-        return jsonify({"error": "TTS conversion failed"}), 500
+    return jsonify({"response": bot_response})
 
 if __name__ == '__main__':
     app.run(debug=True)
